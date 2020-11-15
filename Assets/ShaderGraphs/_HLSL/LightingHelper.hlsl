bc9d8d49a8df0f7760eae6ab73ac948afcad6df4
hironori sugino <git@sugi.cc>
2020-11-16 03:28:02 +0900

    update lightingHelper

--- Assets/ShaderGraphs/_HLSL/LightingHelper.hlsl
+++ Assets/ShaderGraphs/_HLSL/LightingHelper.hlsl
@@ -2,57 +2,6 @@
 //#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Input.hlsl"
 //#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/ShaderGraphFunctions.hlsl"
 
-void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAtten, out half ShadowAtten)
-{
-#ifdef SHADERGRAPH_PREVIEW
-    Direction = normalize(half3(-1,1,-1));
-    Color = half3(1,1,1);
-    DistanceAtten = 1;
-    ShadowAtten = 1;
-#else
-    half cascadeIndex = ComputeCascadeIndex(WorldPos);
-    half4 shadowCoord = mul(_MainLightWorldToShadow[cascadeIndex], float4(WorldPos, 1.0)); //TransformWorldToShadowCoord(WorldPos);
-    Light mainLight = GetMainLight(shadowCoord);
-    Direction = mainLight.direction;
-    Color = mainLight.color;
-    DistanceAtten = mainLight.distanceAttenuation;
- 
-    //ShadowAtten = mainLight.shadowAttenuation;
- 
-    ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
-    half shadowStrength = GetMainLightShadowStrength();
-    ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture,
-            sampler_MainLightShadowmapTexture),
-            shadowSamplingData, shadowStrength, false);
-#endif
-}
-
-void AdditionalLights_half(half3 SpecColor, half Smoothness, half3 WorldPosition, half3 WorldNormal, half3 WorldView, out half3 Diffuse, out half3 Specular)
-{
-#ifdef SHADERGRAPH_PREVIEW
-    Diffuse = 0;
-    Specular = 0;
-#else
-    half3 diffuseColor = 0;
-    half3 specularColor = 0;
- 
-    Smoothness = exp2(10 * Smoothness + 1);
-    WorldNormal = normalize(WorldNormal);
-    WorldView = SafeNormalize(WorldView);
-    int pixelLightCount = GetAdditionalLightsCount();
-    for (int i = 0; i < pixelLightCount; ++i)
-    {
-        Light light = GetAdditionalLight(i, WorldPosition, half4(1,1,1,1));
-        half3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
-        diffuseColor += LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
-        specularColor += LightingSpecular(attenuatedLightColor, light.direction, WorldNormal, WorldView, half4(SpecColor, 0), Smoothness);
-    }
- 
-    Diffuse = diffuseColor;
-    Specular = specularColor;
-#endif
-}
-
 sampler3D _DitherMaskLOD;
 void DitheringShadow_half(half alpha, half2 screenPos, bool shadowCascaded, out half outAlpha, out half clipThreshold)
 {
@@ -68,13 +17,64 @@ void DitheringShadow_half(half alpha, half2 screenPos, bool shadowCascaded, out 
 #endif
 }
 
+#ifdef SHADERGRAPH_PREVIEW
+#else
+half GetAdditionalLightRealtimeShadow(int lightIndex, float3 positionWS)
+{
+    lightIndex = GetPerObjectLightIndex(lightIndex);
+    ShadowSamplingData shadowSamplingData = GetAdditionalLightShadowSamplingData();
+    
+    float4 shadowCoord = mul(_AdditionalLightsWorldToShadow[lightIndex], float4(positionWS, 1.0));
+    half4 shadowParams = GetAdditionalLightShadowParams(lightIndex);
+    shadowCoord.xyz /= shadowCoord.w;
+    half attenuation = SampleShadowmapFiltered(TEXTURE2D_ARGS(_AdditionalLightsShadowmapTexture, sampler_AdditionalLightsShadowmapTexture), shadowCoord, shadowSamplingData);
+    return LerpWhiteTo(attenuation, shadowParams.x);
+}
+//copy function from Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl
+void BlinnPhong(InputData inputData, half3 diffuse, half4 specularGloss, half smoothness,
+    out half3 out_MainDiffuse, out half3 out_MainSpeclur, out half out_MainShadow, out half3 out_AdditionalDiffuse, out half3 out_AdditionalSpaclur)
+{
+    half4 shadowMask = unity_ProbesOcclusion;
+
+    Light mainLight = GetMainLight(inputData.shadowCoord, inputData.positionWS, shadowMask);
+    
+    //Shadow.hlsl
+    ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+    half4 shadowParams = GetMainLightShadowParams();
+    half attenuation = SampleShadowmapFiltered(TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_MainLightShadowmapTexture), inputData.shadowCoord, shadowSamplingData);
+    mainLight.shadowAttenuation = attenuation;
+
+    half3 attenuatedLightColor = mainLight.color * mainLight.distanceAttenuation;
+    out_MainDiffuse = LightingLambert(attenuatedLightColor, mainLight.direction, inputData.normalWS) * diffuse;
+    out_MainSpeclur = LightingSpecular(attenuatedLightColor, mainLight.direction, inputData.normalWS, inputData.viewDirectionWS, specularGloss, smoothness);
+    out_MainShadow = mainLight.shadowAttenuation;
+    
+    out_AdditionalDiffuse = 0;
+    out_AdditionalSpaclur = 0;
+    uint pixelLightCount = GetAdditionalLightsCount();
+    for (uint lightIndex = 0u; lightIndex < pixelLightCount; ++lightIndex)
+    {
+        Light light = GetAdditionalLight(lightIndex, inputData.positionWS, shadowMask);
+        light.shadowAttenuation = GetAdditionalLightRealtimeShadow(lightIndex, inputData.positionWS);
+        
+        half3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
+        out_AdditionalDiffuse += LightingLambert(attenuatedLightColor, light.direction, inputData.normalWS) * diffuse;
+        out_AdditionalSpaclur += LightingSpecular(attenuatedLightColor, light.direction, inputData.normalWS, inputData.viewDirectionWS, specularGloss, smoothness);
+    }
+}
+#endif
+
 void SimpleLit_half(
-half3 positionWS, half3 normalWS,half2 uv1, half2 uv2, half3 viewDirectionWS,
-half3 diffuse, half4 specular, half smoothness, half3 emission,
-out half4 color)
+half3 positionWS, half3 normalWS, half3 viewDirectionWS,
+half3 diffuse, half4 specular, half smoothness,
+out half3 out_MainDiffuse, out half3 out_MainSpeclur, out half out_MainShadow, out half3 out_AdditionalDiffuse, out half3 out_AdditionalSpaclur)
 {
 #ifdef SHADERGRAPH_PREVIEW
-    color = 1;
+    out_MainDiffuse = 1;
+    out_MainSpeclur = 0;
+    out_MainShadow = 1;
+    out_AdditionalDiffuse = 0;
+    out_AdditionalSpaclur = 0;
 #else
     smoothness = exp2(10 * smoothness + 1);
     half4 positionCS = TransformWorldToHClip(positionWS);
@@ -88,11 +88,17 @@ out half4 color)
     inputData.shadowCoord = shadowCoord;
     inputData.fogCoord = ComputeFogFactor(positionCS.z);
     inputData.vertexLighting = half3(0, 0, 0);
-    inputData.bakedGI = shadergraph_LWBakedGI(positionWS, normalWS, uv1, uv2, true);
+    inputData.bakedGI = 0;
     inputData.normalizedScreenSpaceUV = GetNormalizedScreenSpaceUV(positionCS);
     inputData.shadowMask = half4(1, 1, 1, 1);
     
-    color = UniversalFragmentBlinnPhong(inputData, diffuse, specular, smoothness, emission, 1.);
+    BlinnPhong(inputData, diffuse, specular, smoothness, out_MainDiffuse, out_MainSpeclur, out_MainShadow, out_AdditionalDiffuse, out_AdditionalSpaclur);
 #endif
+
+}
+
+void GetFinalColor_half(half3 mainDiffuse, half3 mainSpecular, half mainShadow, half3 additionalDiffuse, half3 additionalSpecular, half3 emission, out half3 out_finalColor)
+{
+    out_finalColor = emission + mainShadow * (mainDiffuse + mainSpecular) + additionalDiffuse + additionalSpecular;
 
 }
